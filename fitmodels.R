4c801acd3f9c6095a09c50117c3b4915e9e45963
Margaret Janiczek <margie.hannum@gmail.com>
2023-05-18 07:03:48 -0400

    completed all models on all 4 scenarios for 50 replications

--- fitmodels.R
+++ fitmodels.R
@@ -9,8 +9,8 @@ set.seed(0203)
 
 
 #df    <- readRDS("data/dat1")
-df    <- readRDS("data2/dat2")
-folder_location <- "data2/scenario2/"
+df    <- readRDS("data2/dat4")
+folder_location <- "data2/scenario4/"
 # Lucas, I loaded dat4 to run the below. Added folder for "scenario4" and saved results there
 source("models.R")
 
@@ -20,26 +20,37 @@ source("models.R")
 #===============================================================================
 start <- Sys.time()
 betas_pclogit <-
-    lapply(c(2:50), \(i) {
+    lapply(c(1:50), \(i) {
       #adding timeout which should produce null if thing takes more than 600 seconds.
       #If it doesn't then it will save the beta results as usual.
     beta  <- withTimeout({pclogit(df[df$rep == i, ], nboot = 200, mc_cores = 20)}, timeout = 2000, onTimeout= "warning")
     if(!is.null(beta)){
       saveRDS(beta, paste0(folder_location,"fit_l/s1_fit_plogit_", i))
-    }
-    print(paste("timeout", i))
+    } else{print(paste("timeout", i))}
     })
 end <- Sys.time()
 end - start
 
-summary(df[df$rep == 13, ])
+#summary(df[df$rep == 13, ])
 # 10  in ~2 min
 # error with c(13, 19, 20, 24, 36)
 
-# scenario 4: error with 3, 6, 7??
-
-#scenario 4:
-# error with 35, 42
+# scenario 4 round 2 time took 8.77 hours with the following timeouts:
+# [1] "timeout 3"
+# [1] "timeout 4"
+# [1] "timeout 7"
+# [1] "timeout 11"
+# [1] "timeout 14"
+# [1] "timeout 16"
+# [1] "timeout 30"
+# [1] "timeout 32"
+# [1] "timeout 36"
+# [1] "timeout 38"
+# [1] "timeout 39"
+# [1] "timeout 42"
+# [1] "timeout 44"
+# [1] "timeout 46"
+# [1] "timeout 48"
 
 list_files <- list.files(paste0(folder_location,"fit_l"), full.names = TRUE)
 list_dat   <- lapply(list_files, \(f) data.frame(readRDS(f)))
@@ -51,14 +62,15 @@ saveRDS(arr, paste0(folder_location,"fit_lasso.rds"))
 #===============================================================================
 start <- Sys.time()
 betas_pclogit <-
-    lapply(2:50, \(i) {
-    beta  <-  bclogit(df[df$rep == i, ],  2, 2000) # changed to 2 chains and 500 iter just for timing for now
+    lapply(1:50, \(i) {
+    beta  <-  bclogit(df[df$rep == i, ],  2, 2000) # changed to 2 chains and 2000 iter just for timing for now
     saveRDS(beta, paste0(folder_location,"fit_b/fit", i))
     })
 
 end <- Sys.time()
 end - start
 
+# did 20 in 15 minutes for scenario 2
 
 # 2 in 30 sec
 # error with 48 (thre are 3 unpaired)
@@ -81,7 +93,7 @@ betas_pclogit <-
 end <- Sys.time()
 end - start
 
-#scenario 2 took 35.6 minutes 
+#scenario 2 took 59.1 minutes 
 
 #scenario 4 took 35.6 minutes
 
